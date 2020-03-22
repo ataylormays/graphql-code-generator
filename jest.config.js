e12a48a8051a7a0370e18bf2472b17b2f3f71b02
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-03-22 21:23:11 +0100

    Use projects

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,4 @@
-const { resolve } = require('path');
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-
-const ROOT_DIR = __dirname;
-const TSCONFIG = resolve(ROOT_DIR, 'tsconfig.json');
-const tsconfig = require(TSCONFIG);
-const CI = !!process.env.CI;
-
 module.exports = {
-  transform: { '^.+\\.tsx?$': 'ts-jest' },
-  testEnvironment: 'node',
-  rootDir: ROOT_DIR,
-  globals: {
-    'ts-jest': {
-      tsConfig: TSCONFIG,
-    },
-  },
-  restoreMocks: true,
-  reporters: ['default'],
-  modulePathIgnorePatterns: ['dist'],
-  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, { prefix: `${ROOT_DIR}/` }),
-  cacheDirectory: resolve(ROOT_DIR, `${CI ? '' : 'node_modules/'}.cache/jest`),
-  collectCoverage: false,
-  // setupFilesAfterEnv: ['<rootDir>/force-gc.js'],
-};
+  rootDir: __dirname,
+  projects: ['<rootDir>/packages/**/*/jest.config.js'],
+}
---
+++ jest.project.js
@@ -0,0 +1,32 @@
+const { resolve } = require('path');
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+
+const ROOT_DIR = __dirname;
+const TSCONFIG = resolve(ROOT_DIR, 'tsconfig.json');
+const tsconfig = require(TSCONFIG);
+const CI = !!process.env.CI;
+
+module.exports = ({ dirname }) => {
+  const pkg = require(resolve(dirname, 'package.json'));
+  const displayName = pkg.name.replace('@graphql-codegen/', '');
+
+  return {
+    displayName,
+    transform: { '^.+\\.tsx?$': 'ts-jest' },
+    testEnvironment: 'node',
+    rootDir: dirname,
+    globals: {
+      'ts-jest': {
+        diagnostics: false,
+        tsConfig: 'tsconfig.json',
+      },
+    },
+    restoreMocks: true,
+    reporters: ['default'],
+    modulePathIgnorePatterns: ['dist'],
+    moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, { prefix: `${ROOT_DIR}/` }),
+    cacheDirectory: resolve(ROOT_DIR, `${CI ? '' : 'node_modules/'}.cache/jest`),
+    collectCoverage: false,
+    setupFilesAfterEnv: [resolve(ROOT_DIR, 'jest.env.js')],
+  };
+};
