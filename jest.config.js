2b8b3f403dce1ec6e67906d143056fddbe953edf
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-03-22 21:58:46 +0100

    Eh

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 const CI = !!process.env.CI;
 
 module.exports = CI
-  ? require('./jest.project')({ __dirname })
+  ? require('./jest.project')({ dirname: __dirname })
   : {
       rootDir: __dirname,
       projects: ['<rootDir>/packages/**/*/jest.config.js'],
